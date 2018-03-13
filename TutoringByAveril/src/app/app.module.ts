40c354183fcf10fe90d7281514776debf38ea5f1
Awsum4sum <jonnobud@gmail.com>
2018-03-13 20:13:59 +0000

    First upload

---
+++ TutoringByAveril/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AlertModule } from 'ngx-bootstrap';
+
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    AlertModule.forRoot(),
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
